eccfc530f66382251c65313be68d75b3563a8fdc
2401968929@qq.com <2401968929@qq.com>
2022-11-11 01:54:25 +0800

    Commit massage

---
+++ src/main/java/com/ql/zqlaini/ZqlainiApplication.java
@@ -0,0 +1,13 @@
+package com.ql.zqlaini;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ZqlainiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ZqlainiApplication.class, args);
+    }
+
+}
